cb3bf77b71bb08faf62b60e361c3a4af3e0e76bf
ll <634019392@qq.com>
2019-07-08 14:59:03 +0800

    update avatar

--- src/utils/api.js
+++ src/utils/api.js
@@ -99,6 +99,29 @@ const refreshToken = async (accessToken) => {
   }
 }
 
+const updateFile = async (options = {}) => {
+  // 显示loading
+  wepy.showLoading({title: '上传中'})
+
+  // 获取 token
+  let accessToken = await getToken()
+
+  // 拼接url
+  options.url = host + '/' + options.url
+  let header = options.header || {}
+  // 将 token 设置在 header 中
+  header.Authorization = 'Bearer' + accessToken
+  options.header = header
+
+  // 上传文件
+  let response = await wepy.uploadFile(options)
+
+  // 隐藏 loading
+  wepy.hideLoading()
+
+  return response
+}
+
 // 登录
 const login = async (params = {}) => {
   // code 只能使用一次，所以每次单独调用
@@ -147,6 +170,7 @@ export default {
   request,
   authRequest,
   refreshToken,
+  updateFile,
   login,
   logout
 }
