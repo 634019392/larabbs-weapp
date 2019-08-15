734b6bdbb8bf7113f74bc14da1f4c67dff6de18b
ll <634019392@qq.com>
2019-08-15 13:56:02 +0800

    user perms

--- src/mixins/replyMixin.js
+++ src/mixins/replyMixin.js
@@ -27,7 +27,7 @@ export default class ReplyMixin extends wepy.mixin {
       // 点击取消后返回
       if (!res.confirm) {
         console.log(this.replies)
-        return;
+        return
       }
       try {
         // 调研用接口删除回复
@@ -106,7 +106,8 @@ export default class ReplyMixin extends wepy.mixin {
       return false
     }
 
-    return (reply.user_id === user.id)
+    // 用户为回复发布者 或 有管理内容权限
+    return (reply.user_id === user.id) || this.$parent.can('manage_contents')
   }
   async onPullDownRefresh() {
     this.noMoreData = false
